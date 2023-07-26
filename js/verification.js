a5eabd9d9b3da19f1460b283ddce9c483deb442e
pb <91002642+pb526@users.noreply.github.com>
2023-07-26 21:12:10 +0800

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -1,7 +1,7 @@
 $('#login-button').click(function (event) {
     let userName = document.getElementById("userName").value;
     let pwd = document.getElementById("pwd").value;
-    if (userName == "陈紫琪" && pwd == "080610") {
+    if (userName == "陈紫琪" && pwd == 080610 ) {
         $('#h').text("欢迎回来！");
         event.preventDefault();
         $('form').fadeOut(500);
